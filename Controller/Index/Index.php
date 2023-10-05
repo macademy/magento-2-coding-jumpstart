20ff359b9bcfeb08c80327524cbd8efaffa21ab3
Mark Shust <mark@shust.com>
2023-10-05 12:21:31 -0400

    Return a standard page response

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace Macademy\Jumpstart\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index implements HttpGetActionInterface
 {
-    public function execute()
+    public function __construct(
+        private PageFactory $pageFactory,
+    ) {}
+
+    public function execute(): Page
     {
-        die('Jumpstart!');
+        return $this->pageFactory->create();
     }
 }
